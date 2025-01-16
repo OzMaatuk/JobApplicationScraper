30214d6a1ca1668c68f67da32ebcf58d42c7aaaf
Oz <ozmaatuk@gmail.com>
2025-01-16 20:43:40 +0200

    improve config values

--- config.py
+++ config.py
@@ -3,25 +3,29 @@
 import os
 import configparser
 
-chrome_user_data_path = os.environ.get("CHROME_USER_DATA")
-
 config = configparser.ConfigParser()
+
+chrome_user_data_path = os.environ.get("CHROME_USER_DATA")
+if not chrome_user_data_path:
+    raise ValueError("CHROME_USER_DATA environment variable is not set.")
+config.read("general", "user_data_path")
+
 config_path = os.environ.get("CONFIG_PATH")
 if not config_path:
     raise ValueError("CONFIG_PATH environment variable is not set.")
-config.read(config_path)
+config.read("general", "config_path")
 
 # Load Credentials: (First from .env, else from config)
 linkedin_username = os.environ.get("LINKEDIN_USERNAME")
 if not linkedin_username:
     linkedin_username = config.get("user_info", "username")
-    if not linkedin_username:
+    if not linkedin_username and not chrome_user_data_path:
         raise ValueError("LINKEDIN_USERNAME is not set in environment variables or config.ini.")
 
 linkedin_password = os.environ.get("LINKEDIN_PASSWORD")
 if not linkedin_password:
     linkedin_password = config.get("user_info", "password")
-    if not linkedin_password:
+    if not linkedin_password and not chrome_user_data_path:
         raise ValueError("LINKEDIN_PASSWORD is not set in environment variables or config.ini.")
 
 # ... other configuration ...
@@ -36,5 +40,6 @@ output_file_name = config.get("general", "output_path")
 # logging configuration
 log_level = config.get("general", "log_level", fallback="DEBUG")
 log_file_path = config.get("general", "log_file", fallback=None)
-import src.logger as LOGGER
-if log_file_path: LOGGER.set_output_path(log_file_path)
+if log_file_path:
+    import src.logger as LOGGER
+    LOGGER.set_output_path(log_file_path)
